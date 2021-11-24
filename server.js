39efcc791e5b4bd216444148541a124a13164da5
Emma <ezpanxy@live.unc.edu>
2021-11-24 18:34:37 -0500

    s

--- server.js
+++ server.js
@@ -15,18 +15,12 @@ var HTTP_PORT = 5000
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
-
-// CREATE (HTTP method POST) at root endpoint /app/
-app.post("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
-	res.status(201);
-});
-
-// READ (HTTP method GET) at root endpoint /app/new/
-app.get("/app/new/", (req, res, next) => {
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
+
 
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
