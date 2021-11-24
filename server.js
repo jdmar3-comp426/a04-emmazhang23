3d033cc6021e3235ae6612524be82fcacda54e26
Emma <ezpanxy@live.unc.edu>
2021-11-24 18:43:10 -0500

    dfs

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?");
-	const info=stmt.run(req.body.user, req.body.pass)
+	const info=stmt.run(req.body.user, md5(req.body.pass))
 	res.status(201).send(info.changes+"record created: ID "+info.lastInsertRowid+ " (201)");
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -45,14 +45,14 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const info=stmt.run(req.params.id, req.body.user, req.body.pass)
-	res.status(200).send(info.changes+"record updated: ID "+req.params.id+ " (200)");
+	const info=stmt.run(md5(req.body.pass), req.body.user, req.params.id)
+	res.status(405).send(info.changes+"record updated: ID "+req.params.id+ " (405)");
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info=stmt.run(req.params.id)
-	res.status(200).send(info.changes+" record deleted: ID "+req.params.id+ " (200)");
+	res.status(405).send(info.changes+" record deleted: ID "+req.params.id+ " (405)");
 });
 // Default response for any other request
 app.use(function(req, res){
