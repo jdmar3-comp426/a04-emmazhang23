48595fae055ef3caef982069d6197b513d864cd5
John D. Martin III <me@johndmart.in>
2021-11-30 11:46:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -37,11 +37,20 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
-	const info=stmt.run(req.params.id)
-	res.status(200).json({"id":parseInt(req.params.id), "user":req.body.user+"", "pass":md5(req.body.pass+"")});
+//app.get("/app/user/:id", (req, res) => {	
+//	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
+//	const info=stmt.run(req.params.id)
+//	res.status(200).json({"id":parseInt(req.params.id), "user":req.body.user+"", "pass":md5(req.body.pass+"")});
+//});
+
+// John's working example
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+	res.json(stmt);
+	res.status(200);
 });
+
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
