e7a7f2bebe515c781e976964525cb55d20bcd61e
Emma <ezpanxy@live.unc.edu>
2021-11-30 10:57:20 -0500

    pulled out user to json

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	const info=stmt.run(req.body.user, md5(req.body.pass))
+	var data={user: req.body.user}
+	const info=stmt.run(data.user, md5(req.body.pass))
 	res.status(201).json({"message": info.changes+" record created: ID "+info.lastInsertRowid+ " (201)"});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
